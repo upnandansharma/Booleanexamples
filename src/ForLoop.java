ad4b9564feb850e78665722fa2f568dcc7979089
upnandansharma <139189231+upnandansharma@users.noreply.github.com>
2023-08-18 18:52:47 -0400

    changes in file

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -59,5 +59,17 @@ public class ForLoop {
 
         }
     }
-}
+    public static void sumOfFibonocciSeries(){
+        int n1= 0;
+        int n2= 1;
+        System.out.println(n1);
+        System.out.println(n2);
+        int next =0;
+        for (int i = 1; i <=8 ; i++) {
+            next= n1+n2;
+            System.out.println(next);
+            n1=n2;
+            n2=next;
+
+        }
 
